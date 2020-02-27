37027555fc089ff482e04819fd12292edc21f3fc
ow-tech <wagwanwekon@outloo.com>
2020-02-27 12:11:36 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// //user interface
+// $(document).ready(function(){
+//     $(function(){
+//         $(".carousel").carousel({
+//             interval: 1000*1 // 1 sec.
+//         })
+//     });
+// })
