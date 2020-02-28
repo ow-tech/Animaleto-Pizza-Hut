328b9417cc5e966d78b49bdfda59a5e756855874
ow-tech <wagwanwekon@outloo.com>
2020-02-28 16:50:38 +0300

    construction of delivery function

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,16 @@ $(document).ready(function(){
 		// 	return parseInt(psize) + parseInt(crust) + parseInt(toppings)
 		// }) 
 		var total= (parseInt(psize) + parseInt(crust) + parseInt(toppings))*parseInt(number);
-		$("p#pizza").html ("So far, your pizza costs you Ksh.g" + total );
+		$("p#pizza").html ("So far, your pizza costs you Ksh." + total );
 	});
+	$("button#deliver").click(function(){
+		alert("delivery cost Ksh. 200.")
+		var where= prompt("Please Enter your location:\n Must Be within Mombasa.")
+		alert("Your Pizza will be delivered to "+ where + " in about 1 hour.\n Please CheckOut to get total price and for your order to be processed")
+	})
+
+	$("button#no").click(function(){
+		alert("Please CheakOut for your order to be processed")
+	})
 
 })
