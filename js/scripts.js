cf954810e06002a889829d80e003f494bfa9b6cc
ow-tech <wagwanwekon@outloo.com>
2020-02-28 17:17:21 +0300

    construction of checkout function

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,29 @@ $(document).ready(function(){
 		// }) 
 		var total= (parseInt(psize) + parseInt(crust) + parseInt(toppings))*parseInt(number);
 		$("p#pizza").html ("So far, your pizza costs you Ksh." + total );
-	});
-	$("button#deliver").click(function(){
-		alert("delivery cost Ksh. 200.")
-		var where= prompt("Please Enter your location:\n Must Be within Mombasa.")
-		alert("Your Pizza will be delivered to "+ where + " in about 1 hour.\n Please CheckOut to get total price and for your order to be processed")
-	})
 
-	$("button#no").click(function(){
-		alert("Please CheakOut for your order to be processed")
-	})
+		$("button#deliver").click(function(){
+			alert("delivery cost Ksh. 200.")
+			var where= prompt("Please Enter your location:\n Must Be within Mombasa.")
+			alert("Your Pizza will be delivered to "+ where + " in about 1 hour.\n Please CheckOut to get total price and for your order to be processed")
+		})
+		// $ ("button#checkout").click(function(){
+		// 	alert ( total + 200)
+		// })
+		// $("button#no").click(function(){
+		// 	alert(total)
+		// })
+		$("button#checkout").click(function(){
+			if(where.length>1){
+				alert (total+200);
+			}else{
+				alert (total);
+			}
+		})
+	});
+
+	// $("button#checkout").click(function(){
+	// 	if(where.length>1)
+	// })
 
 })
