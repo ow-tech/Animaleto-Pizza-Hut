f1087f37ec400367d831e3c0242e22fbef8b74b2
ow-tech <wagwanwekon@outloo.com>
2020-02-28 14:21:00 +0300

    construction of jquery function to handle the size, toppings & crust of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-// //user interface
-// $(document).ready(function(){
-//     $(function(){
-//         $(".carousel").carousel({
-//             interval: 1000*1 // 1 sec.
-//         })
-//     });
-// })
+$(document).ready(function(){
+	$("button#takeorder").click(function(){
+		var psize=$("input[type='radio']:checked").val();
+		var crust =$(".custom-select option:selected").val();
+		var toppings =$ ("input[type='checkbox']:checked").val();
+		$("p#pizza").html(psize + crust+ toppings);
+	});
+	
+})
