d6da628f5b89ac09e78d8283f0f1bd6f1c9e621d
ow-tech <wagwanwekon@outloo.com>
2020-02-29 10:18:40 +0300

    reconstruction of my functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,37 @@ $(document).ready(function(){
 		var crust =$(".custom-select option:selected").val();
 		var toppings =$ ("input[type='checkbox']:checked").val();
 		var number =$("#getnumber option:selected").val();
-		// var total=$(function(){
-		// 	return parseInt(psize) + parseInt(crust) + parseInt(toppings)
-		// }) 
 		var total= (parseInt(psize) + parseInt(crust) + parseInt(toppings))*parseInt(number);
 		$("p#pizza").html ("So far, your pizza costs you Ksh." + total );
 
 		$("button#deliver").click(function(){
 			alert("delivery cost Ksh. 200.")
 			var where= prompt("Please Enter your location:\n Must Be within Mombasa.")
-			alert("Your Pizza will be delivered to "+ where + " in about 1 hour.\n Please CheckOut to get total price and for your order to be processed")
-		})
-		// $ ("button#checkout").click(function(){
-		// 	alert ( total + 200)
-		// })
-		// $("button#no").click(function(){
-		// 	alert(total)
-		// })
-		$("button#checkout").click(function(){
-			if(where.length>1){
-				alert (total+200);
-			}else{
-				alert (total);
+			if(where===""){
+				alert("please Enter your Location!!!\n By Clicking YES Button Again!!!")
+			}
+			else{
+				alert("Your Pizza will be delivered to "+ where + " in about 1 hour.\n Please CheckOut to get total price and for your order to be processed")
 			}
 		})
-	});
+		$("button#no").click(function(){
+			alert(" Please Checkout for your order to be Processed")
+		})
+		$("button#checkout").click(function(){
+			var finalTotal= total+200
+		 if(where.length){
+				alert ("Pizza Costs Ksh." +finalTotal);
+			}
+		 else{
 
-	// $("button#checkout").click(function(){
-	// 	if(where.length>1)
-	// })
+			}
+			// if(where.length>1){
+			// 	alert ("Pizza Costs "+total+200);
+			// }
+			// else{
+			// 	alert (total);
+			// }
+		})
+	});
 
 })
